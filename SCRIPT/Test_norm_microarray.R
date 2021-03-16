5169dfd5d8fb70172d03b9dff46388711a309193
acolajanni <antonin21000@gmail.com>
2021-03-16 12:09:10 +0100

    oubli du dernier commit

--- SCRIPT/Test_norm_microarray.R
+++ SCRIPT/Test_norm_microarray.R
@@ -15,7 +15,7 @@ mydata1 <- madsim(mdata = NULL, n = 100, ratio = 0, fparams, dparams, sdn, rseed
 micro <- as.data.frame(mydata1$xdata)
 micro
 
-data = read.csv("~/GIT/CPRD/DATA/MICROARRAYS/Simulmicroarrays.csv",header = T, sep = ',')
+data = read.csv("~/GIT/CPRD/DATA/MICROARRAYS/Simulmicroarrays.csv",row.names = 1,header = T, sep = ',')
 data
 targets = readTargets("~/GIT/CPRD/DATA/MICROARRAYS/Simulmicroarrays.csv", row.names= 1, sep = ',')
 targets
@@ -25,8 +25,12 @@ targets
 help(readTargets)
 help(read.maimages)
 
-RG = read.maimages(targets, source = 'agilent', wt.fun = F)
+RG = read.maimages(targets, source = 'agilent', green.only = T)
 
+# Je n'arrive pas à utiliser limma pour avoir le type d'objet dont limma veut pour normaliser à partir de madsim
+# Je ne sais pas quoi faire du coup... 
+#je vais essayer avec l'autre type de données : Affymetrix (voir tableau partagé)
 
-help("madsim")
+BiocManager::install("gcrma")
+library('gcrma')
 
