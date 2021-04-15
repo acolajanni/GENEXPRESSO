650e4a6ad3bb7c961830155dee90fbf9aff59316
acolajanni <antonin21000@gmail.com>
2021-04-15 08:50:57 +0200

    fonction DEG limma + make design matrix

--- SCRIPT/geolimma.R
+++ SCRIPT/geolimma.R
@@ -17,6 +17,26 @@ Micro = read.csv("~/GIT/CPRD/DATA/MICROARRAYS/Simulmicroarrays.csv", header = TR
 
 Nano = read.csv("~/GIT/CPRD/DATA/NANOSTRING/NanoNormNappa.csv", header = TRUE,row.names = 1)
 
+make_designMatrix <- function(dataset,cond1 = "Control", cond2 = "Test"){
+  status.control = rep(cond1,ncol(dataset)/2)
+  status.test = rep(cond2,ncol(dataset)/2)
+  status = c(status.control,status.test)
+  design = model.matrix(~0+status)
+  colnames(design) <- c(cond1,cond2)
+  return(design)
+}
+
+DEG_limma <- function(dataset,design){
+  fit <- lmFit(Micro,design)
+  cm <- makeContrasts(diff = Control-Test,levels=design)
+  fit2 <- contrasts.fit(fit, cm)
+  fit2 <- eBayes(fit2)
+  res.diff <- topTable(fit2, coef="diff",genelist=row.names(dataset), number=Inf)
+  res.diff_limma <- data.frame(PValue=(res.diff$adj.P.Val),SYMBOL=res.diff$ID)
+  colnames(res.diff_limma) <- c("Limma","Gene.ID")
+  return(res.diff_limma)
+}
+
 #________________________________________________ 
 #Limma : 
 # Nano : Déja implémenté
@@ -26,11 +46,7 @@ Nano = read.csv("~/GIT/CPRD/DATA/NANOSTRING/NanoNormNappa.csv", header = TRUE,ro
 # Microarray : (extrait de functions.R)
 
 # construction de la matrice de design
-status.control = rep("Control",ncol(Micro)/2)
-status.test = rep("Test",ncol(Micro)/2)
-status = c(status.control,status.test)
-design = model.matrix(~0+status)
-colnames(design) <- c("Control","Test")
+design = make_designMatrix(dataset = Micro)
 
 #Application du modèle linéaire:
 fit <- lmFit(Micro,design)
@@ -47,11 +63,7 @@ tail(res.diff_limma)
 # GEOlimma sur Micro array
 
 # construction de la matrice de design
-status.control = rep("Control",ncol(Micro)/2)
-status.test = rep("Test",ncol(Micro)/2)
-status = c(status.control,status.test)
-design = model.matrix(~0+status)
-colnames(design) <- c("Control","Test")
+design = make_designMatrix(dataset = Micro)
 
 # Lignes de codes issues de DE_source.R : Application du modèle linéaire
 cont.matrix <- makeContrasts(compare="Control-Test",levels=design)
@@ -63,3 +75,7 @@ res.diff_geolimma <- data.frame(PValue=(de$adj.P.Val),genes=row.names(de))
 colnames(res.diff_geolimma) <- c("GEOlimma","Gene.ID")
 head(res.diff_geolimma)
 tail(res.diff_geolimma)
+
+
+
+  
