815564dcfd0613fee5b9a525e357319bdbaa398d
Juliette Casemajor <juliette.tardivycasemajor@gmail.com>
2021-04-15 19:49:19 +0200

    Ajout commentaires

--- SCRIPT/Wilcox.R
+++ SCRIPT/Wilcox.R
@@ -21,12 +21,12 @@ micro
 #On peut passer à test de wilcoxon (Test U) : comparaison de deux echantillons independants
 #On a 100 genes et deux groupes de patients (2x6)
 
-#Creation d'un dataframe vide
+#Creation des dataframes vides
 wilcoxtsmicro <- data.frame()
 wilcoxlmicro <- data.frame()
 wilcoxgmicro <- data.frame()
 
-#Test de wilcoxon effectué sur chaque ligne du dataframe
+#Test de wilcoxon two-sided effectué sur chaque ligne du dataframe
 #Chaque valeur de pvalue est extraite puis rajoutée à un tableau de resultats
 for (i in 1:nrow(micro)) {
   x = micro[i,1:6]
@@ -36,14 +36,23 @@ for (i in 1:nrow(micro)) {
   wilcoxtsmicro <- rbind(wilcoxtsmicro,pval)
 }
 
+#Test de wilcoxon less effectue sur chaque ligne du dataframe :
+#On fait l'hypothese que l'expression du groupe 1 est inferieure a celle du groupe 2
 for (i in 1:nrow(micro)) {
+  #Selection du 1er groupe de patients
   x = micro[i,1:6]
+  #Selection du deuxieme groupe de patients
   y = micro[i,7:12]
+  #Test
   test <- wilcox.test(as.numeric(x),as.numeric(y),alternative="less")
+  #On recupere la pvalue
   pval <- test$p.value
+  #On l'ajoute au tableau
   wilcoxlmicro <- rbind(wilcoxlmicro,pval)
 }
 
+#Test de wilcoxon greater effectue sur chaque ligne du dataframe :
+#On fait l'hypothese que l'expression du groupe 1 est superieure a celle du groupe 2
 for (i in 1:nrow(micro)) {
   x = micro[i,1:6]
   y = micro[i,7:12]
@@ -52,8 +61,10 @@ for (i in 1:nrow(micro)) {
   wilcoxgmicro <- rbind(wilcoxgmicro,pval)
 }
 
+#On fusionne les tableaux par les colonnes
 wilcoxmicro <- cbind(wilcoxtsmicro,wilcoxlmicro,wilcoxgmicro)
-#Changement du nom de la colonne en p-values
+
+#Changement du nom des colonnes selon le type de test
 colnames(wilcoxmicro)[1] <- "wilcox two-sided"
 colnames(wilcoxmicro)[2] <- "wilcox less"
 colnames(wilcoxmicro)[3] <- "wilcox greater"
