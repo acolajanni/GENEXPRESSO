99088e08be44dd9c03f586d6a7d358ca5372aad8
Antonin Colajanni <antonin21000@gmail.com>
2021-06-01 14:19:54 +0200

    commit

--- analyse/GSE.R
+++ analyse/GSE.R
@@ -81,10 +81,43 @@ subset(attributes, attributes$name == test[1] | attributes$name == test[2])
 # symbol human gene nomenclature comitee / uniprot symbol
 
 affy_ensembl = c("affy_hg_u133_plus_2", "ensembl_gene_id", "uniprot_gn_symbol") # regarder uniprot symbol
+
+
+#################
+#################
+#################
+
 # si erreur getnodeset() : https://www.ensembl.org/info/website/archives/index.html le site est down
-BM = getBM(attributes = affy_ensembl, mart = ensembl, uniqueRows = TRUE, useCache = FALSE)
+
+BM = getBM(attributes = affy_ensembl, 
+           mart = ensembl, uniqueRows = TRUE, useCache = FALSE
+           )
 # On récupère la liste des sondes réellement présentes dans la bdd
 BM2 = subset(BM, BM$affy_hg_u133_plus_2 != "")
+
+#################
+#################
+#################
+
+# Au final on garde le max() ==> à voir ce qu'on garde (mean, median, ...)
+Probesets=paste("a",1:200,sep="") # "fake probesets"
+Genes=sample(letters,200,replace=T)
+Genes
+Value=rnorm(200)
+
+
+
+
+
+X=data.frame(Probesets,Genes,Value)
+X=X[order(X$Value,decreasing=T),]
+Y=X[which(!duplicated(X$Genes)),]
+
+
+
+
+
+
 
 ############################################
 ##              avec hgu.db               ##
@@ -108,38 +141,164 @@ k = keys(hgu133plus2.db,keytype="PROBEID")
 
 Probes = select(hgu133plus2.db, keys=k, columns="SYMBOL", keytype="PROBEID")
 
+
+ls(hgu133plus2SYMBOL)[mget(ls(hgu133plus2SYMBOL), hgu133plus2SYMBOL) %in% probeName$PROBEID]
+
+
 #### Test de mapping : 
 
 # On récupère la liste des sondes de notre échantillon
 probeName = data.frame(PROBEID = row.names(expr.val))
 
-library(dplyr)
+library("hgu133plus2.db")
+x <- hgu133plus2SYMBOL
+Llength(x)
+# Nombre / sondes de sondes mappés : 
+count.mappedkeys(x)
+nhit(x)
+# nb mappé / non mappé
+# non mappé : 10344
+# mappé 1fois : 41922
+# mappé plus d'une fois :
+table(nhit(toggleProbes(hgu133plus2SYMBOL, "all")))
+table(nhit(x))
+
+?toggleProbes
+
+probes.ALL=row.names(expr.val)
+test = unlist(mget(probes.ALL,  ))
 
 
-df1[, names(df1) %in% names(df2)]
-df1[, names(df2) %in% names(df1)]
 
+# ALIAS2PROBE mapping
 
-
-test = Probes$PROBEID[Probes$PROBEID %in% probeName$PROBEID]
-
-https://www.biostars.org/p/61987/
-
-test = rbind(probeName,Probes)
 
 # Sauvegarder l'avancée actuelle
 # save.image(file = "./GSE31684.RData")
 
 
 ###############"
+hgu133plus2.annot = read.csv("./data/HG-U133_Plus_2.na36.annot.csv", skip = 25, header=T, na.strings ="---")
+
+
+ID.ALL = subset(hgu133plus2.annot, select = c("Probe.Set.ID","Gene.Symbol") )
+
+
+
 #Remove control probes
 # Trouver les sondes controles
-expr.val.ALL=expr.val[1:12065,] #Remove Affy control probes, custom CDF
+#Extract the control probes
+expr.val = as.data.frame(expr.val)
+ControlProbes <- grep("AFFX",row.names(expr.val))
 
+
+#remove control : 
+Without.control.probe=expr.val[-ControlProbes,]
+# Nb de sondes contrôles : 
+nrow(expr.val[ControlProbes,])
+
+# On récupère nos noms de sondes
 probes.ALL=row.names(expr.val)
+# Les symboles qui vont avec : 
 symbol.ALL = unlist(mget(probes.ALL, hgu133plus2SYMBOL))
 ID.ALL = unlist(mget(probes.ALL, hgu133plus2UNIPROT))
+ID.ALL = as.data.frame(ID.ALL)
 
-table.ALL=cbind(probes.ALL,ID.ALL,symbol.ALL,expr.val)
+test = as.data.frame(unlist(mget(probes.ALL, hgu133plus2REFSEQ)))
 
-                
+
+# construction du tableau contenant les Symboles des gènes + ID des sondes + les données brutes
+table.ALL=cbind(PROBES = probes.ALL,
+                #ID.ALL,
+                SYMBOL = symbol.ALL,
+                expr.val)
+
+# On récupère la liste des échantillons
+samples = samples[!samples %in% c("PROBES","SYMBOL")]
+
+#X = table.ALL
+#X=X[order(X$values,decreasing=T),]
+#Y=X[which(!duplicated(X$SYMBOL)),]
+
+
+# En parallèle on créer un DF contenant les ID des sondes et des gènes
+table.ID = data.frame(row.names = row.names(table.ALL),
+                      PROBES = table.ALL$PROBES,
+                      SYMBOL = table.ALL$SYMBOL)
+
+table.ID = data.frame(row.names = ID.ALL$Probe.Set.ID, SYMBOL = ID.ALL$Gene.Symbol, PROBES = ID.ALL$Probe.Set.ID)
+
+# On enlève tous les ID dupliqués
+ID.unique = table.ID[which(!duplicated(table.ID$SYMBOL)),]
+ID.unique = ID.unique[order(ID.unique$SYMBOL),]
+
+
+#ID.unique = merge(ID.unique, Y)
+
+for (i in 1:length(samples)){
+  SampleName = samples[i]
+  # On récupère un DF avec un échantillon uniquement
+  SampleValue = subset(x = table.ALL, select = c(SampleName,"SYMBOL") )
+  
+  # On réordonne les valeurs du plus grand au plus petit
+  X=SampleValue[order(SampleValue[SampleName],decreasing=T),]
+  # On enlève les lignes où le Symbole est dupliqué
+  # On enlève donc les lignes dont les valeurs sont inférieurs au max
+  Y=X[which(!duplicated(SampleValue$SYMBOL)),]
+  Y=Y[order(Y$SYMBOL),]
+  
+  ID.unique = cbind(ID.unique,Y[SampleName])
+  #ID.unique = merge(ID.unique, Y, by = "SYMBOL")
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#############################################"""
+#############################################"""
+y = hgu133plus2UNIPROT[isNA(hgu133plus2UNIPROT)]
+Unmapped = Lkeys(y)    
+ID.Unmapped = unlist(mget(Unmapped, hgu133plus2UNIPROT))
+
+ID.mapped = ID.ALL[!ID.ALL %in% ID.Unmapped]
+#############################################"""
+#############################################"""
+
+
+
+
+
+
+
+
+#############################################"""
+#############################################"""
+## How many probes?
+dim(hgu133plus2UNIPROT)
+##Make a mapping with multiple probes exposed
+multi <- toggleProbes(hgu133plus2UNIPROT, "all")
+singleOnly <- toggleProbes(hgu133plus2UNIPROT, "single")
+## How many probes?
+dim(multi) #on a donc environ 7000 sondes qui mappent le même gene
+dim(singleOnly)
+
+hasSingleProbes(singleOnly)
+#############################################"""
+#############################################"""
